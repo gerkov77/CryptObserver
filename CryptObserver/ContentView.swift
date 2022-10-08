fd09146ef6a5ee9f5b618277e25015a07ec7deae
Gergely Kovács <gerkov77@gmail.com>
2022-10-08 10:06:07 +0200

    Initial Commit

---
+++ CryptObserver/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CryptObserver
+//
+//  Created by Gergely Kovacs on 2022. 10. 08..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptObserver/CryptObserverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptObserverApp.swift
+//  CryptObserver
+//
+//  Created by Gergely Kovacs on 2022. 10. 08..
+//
+
+import SwiftUI
+
+@main
+struct CryptObserverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
