e4121926151c5ffe9ca3600bd23926e63e15394e
Gergely Kovács <gerkov77@gmail.com>
2022-10-08 14:59:55 +0200

    Empty state view connected, shadow correction

--- CryptObserver/AssetsListScreen.swift
+++ CryptObserver/AssetsListScreen.swift
@@ -15,31 +15,39 @@ struct AssetsListScreen: View {
 
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVStack {
-                    ForEach(viewModel.assets, id: \.id) { asset in
-                        CryptoCurrencyCell(
-                            name: asset.name,
-                            symbol: asset.symbol,
-                            image: asset.imageUrlString,
-                            price: asset.priceString,
-                            changePercentage: asset.changePercentString,
-                            priceDropped: asset.didDropPrice,
-                            isExpanded: .constant(false))
-                            .padding(.horizontal, 16)
-                            .padding(.vertical, 4)
-                            .shadow(radius: 10)
+            Group {
+
+                if viewModel.assets.isEmpty {
+                    EmptyStateView()
+                } else {
+                    ScrollView {
+                        LazyVStack {
+                            ForEach(viewModel.assets, id: \.id) { asset in
+                                CryptoCurrencyCell(
+                                    name: asset.name,
+                                    symbol: asset.symbol,
+                                    image: asset.imageUrlString,
+                                    price: asset.priceString,
+                                    changePercentage: asset.changePercentString,
+                                    priceDropped: asset.didDropPrice,
+                                    isExpanded: .constant(false))
+                                    .padding(.horizontal, 16)
+                                    .padding(.vertical, 4)
+                                    .shadow(radius: 4)
+                            }
+                        }
+                        .searchable(text: $searchtext)
                     }
                 }
-                .searchable(text: $searchtext)
             }
             .background {
                 Color(uiColor: .systemGray5)
+        }
+            .onAppear {
+                viewModel.fetchAssets()
             }
         }
-        .onAppear {
-            viewModel.fetchAssets()
-        }
+
     }
 }
 
